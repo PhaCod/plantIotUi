680ff671ff83d3235e33b49a0bf3b6643fe7f6b7
vmanhhh <108534427+vmanhhh@users.noreply.github.com>
2025-04-23 01:10:53 +0700

    Update

--- app/api/mail-subscribe/route.ts
+++ app/api/mail-subscribe/route.ts
@@ -1,4 +1,4 @@
 const API_BASE_URL =
     process.env.NEXT_PUBLIC_API_BASE_URL || "http://127.0.0.1:5000";
 const API_FEED_URL = process.env.NEXT_PUBLIC_API_DIRECT;
-const PUBLIC_API_KEY = process.env.API_KEY;
+const PUBLIC_API_KEY = process.env.NEXT_PUBLIC_API_KEY;
